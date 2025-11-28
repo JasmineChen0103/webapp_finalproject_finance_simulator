720c759b252d9f222c0fe989665efeaeb503579b
Ethan WU <h3240281370@gmail.com>
2025-11-28 20:13:45 +0800

    前後端串接

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 try:
 	from .routers.SimulationApi import router as simulation_router
@@ -11,6 +12,13 @@ except ImportError:  # Allow running ``uvicorn main:app`` from the app folder
 
 
 app = FastAPI(title="Finance Simulation API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],          
+    allow_credentials=True,
+    allow_methods=["*"],          
+    allow_headers=["*"],
+)
 
 
 app.include_router(simulation_router)
